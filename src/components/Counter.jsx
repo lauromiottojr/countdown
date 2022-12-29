170af041324dcfd4a1798f93f532bc5a24ff72ec
lauromiottojr <junior.miotto36@gmail.com>
2022-12-29 09:07:46 -0300

    Initial structure

---
+++ src/components/Counter.jsx
@@ -0,0 +1,10 @@
+const Counter = ({ title, number }) => {
+    return (
+        <div className="counter">
+            <p className="counterNumber">{number}</p>
+            <h3 className="counterText">{title}</h3>
+        </div>
+    )
+}
+
+export default Counter
